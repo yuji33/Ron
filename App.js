21aa70e1a39c0605cd3439d81c474f823d7171a2
高津裕二 <yipusan33@gmail.com>
2021-09-16 18:51:09 +0900

    add TabScreen

--- App.js
+++ App.js
@@ -3,15 +3,21 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <SafeAreaProvider>
        <NavigationContainer>
-        <View style={styles.container}>
-         <Text>プログラミング</Text>
-         <StatusBar style="auto" />
-        </View>
+        <Tab.Navigator>
+         <Tab.Screen name="Home" component={HomeScreen} />
+         <Tab.Screen name="Settings" component={SettingsScreen} />
+        </Tab.Navigator>
+        {/* <View style={styles.container}> */}
+         {/* <Text>プログラミング</Text> */}
+         {/* <StatusBar style="auto" /> */}
+        {/* </View> */}
        </NavigationContainer>
     </SafeAreaProvider>
   );
@@ -25,3 +31,19 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
